1075dc54d82be6416e81f93d5b30cd4e4cec6d69
Kozlov Valentin <kozlov0020@gmail.com>
2023-06-14 16:10:43 +0500

    add packApi

---
+++ src/features/packs/packsApi.ts
@@ -0,0 +1,64 @@
+import { instance } from "../../common/api/common.api";
+
+export const packsApi={
+  getPacks:(arg:GetPacksArg)=>{
+return instance.get<any>('cards/pack',{params:{...arg}})
+},
+  addPack:(arg:AddPackArg)=>{
+    return instance.post('cards/pack')
+  },
+  deletePack:(id:string)=>{
+    return instance.delete( `/cards/pack?id=${id}`)
+  },
+  updatePack:(arg:UpdatePackArg)=>{
+    return instance.put('cards/pack',{cardsPack:{...arg}})
+  }
+}
+
+//Types
+type GetPacksArg={
+  packName?: string;
+  min?: number;
+  max?: number;
+  sortPacks?: string;
+  page?: number;
+  pageCount?: number;
+  user_id?: string;
+}
+type PacksResType={
+  cardPacks: CardPackType[];
+  page: number; //выбранная страница
+  pageCount: number; //количество эл-ов на странице
+  cardPacksTotfalCount: number; //количество колод
+  minCardsCount: number;
+  maxCardsCount: number;
+  token: string;
+  tokenDeathTime: number;
+}
+export type CardPackType = {
+  _id: string;
+  user_id: string;
+  user_name: string;
+  private: boolean;
+  name: string;
+  path: string;
+  grade: number;
+  shots: number;
+  deckCover: string;
+  cardsCount: number;
+  type: string;
+  rating: number;
+  created: string;
+  updated: Date;
+  more_id: string;
+  __v: number;
+};
+type AddPackArg={
+  name?: string; // если не отправить будет "no Name"
+  deckCover?: string; // не обязателен  url/base64
+  private?: boolean; // если не отправить будет false
+}
+export type UpdatePackArg = {
+  _id: string;
+  name?: string;
+};
